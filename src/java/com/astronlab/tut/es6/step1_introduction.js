ce60018ab06d17898105b7a322947d5dc95d50ac
arjuda <arjuda.anjum@example.com>
2017-08-09 18:05:17 +0600

    added other files

---
+++ src/java/com/astronlab/tut/es6/step1_introduction.js
@@ -0,0 +1,19 @@
+
+/*
+ Introduction:
+================
+ 
+ ES6 (aka Ecma Script 2015) introduces Java/Groovy like OOP syntax (Class, Polymorphism, etc) and other keywords. However, it
+ actually uses old (ES5) structure(prototype) to acheve OOP achitecture. So before diving into ES6, lets see what is classic "prototype" 
+ and how it works.
+
+ Protype:
+==========
+- Its a template mechanism, like a java "class", from which we create "new" instances.
+- Created "objects" mantains a "chain/link" to its "parent", its called "prototype chain".
+- Prototype of an object is denoted via: "__poto__"
+- child_object.__poto__ === Parent.
+
+https://reinteractive.com/posts/235-es6-classes-and-javascript-prototypes
+http://adrianmejia.com/blog/2016/10/19/Overview-of-JavaScript-ES6-features-a-k-a-ECMAScript-6-and-ES2015/
+ */
---
+++ src/java/com/astronlab/tut/servlet/DemoCss3.java
@@ -0,0 +1,21 @@
+package com.astronlab.tut.servlet;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "DemoCss3", urlPatterns = {"/demo"})
+public class DemoCss3 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        RequestDispatcher view = request.getRequestDispatcher("demo/demo_ball.html");
+        view.forward(request, response);
+    }
+}
